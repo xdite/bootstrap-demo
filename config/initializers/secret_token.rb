0ff06d9703f452ace954448e071efd6d5f38ca2c
xdite <xdite@rocodev.com>
2012-07-04 23:54:09 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootstrapDemo::Application.config.secret_token = '03e06e9c4538511cd604f342c5bf5e4ce5be3029954f4142ccfb7187d016e9ae19ed9feb480f0992d1623813b0933f31a52c645c295931bc7472e334dbb0d7e5'
